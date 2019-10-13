c3c8e0e1ed7519d299cc279aec2bd177f597481c
maciejkryger <snowyman1979@gmail.com>
2019-10-13 11:15:56 +0200

    Spring Core - Calculator - classwork

--- src/main/java/pl/sda/spring/operation/OperationConfiguration.java
+++ src/main/java/pl/sda/spring/operation/OperationConfiguration.java
@@ -4,10 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-class SubtractionOperationConfiguration {
+public class OperationConfiguration {
 
     @Bean
     SubtractionOperation subtractionOperation() {
         return new SubtractionOperation();
     }
+
+    @Bean
+    PercentageOperation percentageOperation() {return new PercentageOperation();}
 }
--- src/test/java/pl/sda/spring/CalculatorTest.java
+++ src/test/java/pl/sda/spring/CalculatorTest.java
@@ -27,4 +27,104 @@ public class CalculatorTest {
         //then
         Assert.assertEquals(5.0, actual, 0.00000001);
     }
+
+    @Test
+    public void shouldReturnOneForGivenArgumentsOnSUBTRACTIONOperation(){
+        //given
+        double x = 3;
+        double y = 2;
+
+        //when
+        double actual = calculator.calculate(OperationType.SUBTRACTION, x, y);
+
+        //then
+        Assert.assertEquals(1,actual,0.000000001);
+    }
+
+    @Test
+    public void shouldReturnSixForGivenArgumentOnMultiplicationOperation(){
+
+        //given
+        double x = 3;
+        double y = 2;
+
+        //when
+        double actual = calculator.calculate(OperationType.MULTIPLICATION,x,y);
+
+        //then
+        Assert.assertEquals(6,actual,0.000000001);
+
+    }
+
+
+    @Test
+    public void shouldReturnThreeForGivenArgumentOnDivisionOperation(){
+
+        //given
+        double x = 6;
+        double y = 2;
+
+        //when
+        double actual = calculator.calculate(OperationType.DIVISION,x,y);
+
+        //then
+        Assert.assertEquals(3,actual,0.000000001);
+
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void shouldReturnExceptionForGivenArgumentOnDivisionOperation(){
+
+        //given
+        double x = 6;
+        double y = 0;
+
+        //when
+        double actual = calculator.calculate(OperationType.DIVISION,x,y);
+    }
+
+    @Test
+    public void shouldReturnTenForGivenArgumentOnDivisionOperation(){
+
+        //given
+        double x = 100;
+        double y = 10;
+
+        //when
+        double actual = calculator.calculate(OperationType.PERCENTAGE,x,y);
+
+        //then
+        Assert.assertEquals(10,actual,0.000000001);
+    }
+
+    @Test
+    public void shouldReturnTwentyFiveForGivenArgumentOnPowerOperation(){
+
+        //given
+        double x = 5;
+        double y = 3;
+
+        //when
+        double actual = calculator.calculate(OperationType.POWER,x,y);
+
+        //then
+        Assert.assertEquals(125,actual,0.000000001);
+    }
+
+
+    @Test
+    public void shouldReturnOneForGivenArgumentOnPowerOperation(){
+
+        //given
+        double x = 5;
+        double y = 0;
+
+        //when
+        double actual = calculator.calculate(OperationType.POWER,x,y);
+
+        //then
+        Assert.assertEquals(1,actual,0.000000001);
+    }
+
+
 }
